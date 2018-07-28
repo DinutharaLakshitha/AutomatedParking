7e5fa15e2962858c067eddaf92a75874a59f6971
Lakshitha Rajapakse <lakshitha.15@cse.mrt.ac.lk>
2018-07-28 13:18:02 +0530

    ultrasonic updated

--- ultrsonic/ultrsonic.ino
+++ ultrsonic/ultrsonic.ino
@@ -6,55 +6,139 @@
 *
 */
 // defines pins numbers
-const int trigPin = 9;
-const int echoPin = 10;
+const int front_sen_one_trigPin = 2;
+const int front_sen_one_echoPin = 3;
+
+//const int front_sen_two_trigPin = 4;
+const int front_sen_two_echoPin = 4;
+
+//const int back_sen_one_trigPin = 6;
+const int back_sen_one_echoPin = 5;
+
+//const int back_sen_two_trigPin = 8;
+const int back_sen_two_echoPin =6;
+
+
 // defines variables
-long duration;
-int distance;
+long front_sen_one_duration;
+long front_sen_two_duration;
+long back_sen_one_duration;
+long back_sen_two_duration;
+
+int front_sen_one_distance;
+int front_sen_two_distance;
+int back_sen_one_distance;
+int back_sen_two_distance;
+
 int vehicalCount;
+
 void setup() {
-pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(front_sen_one_trigPin, OUTPUT); // Sets the trigPin as an Output
+//pinMode(front_sen_two_trigPin, OUTPUT);
+//pinMode(back_sen_one_trigPin, OUTPUT);
+//pinMode(back_sen_two_trigPin, OUTPUT);
+
+pinMode(front_sen_one_echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(front_sen_two_echoPin, INPUT);
+pinMode(back_sen_one_echoPin, INPUT);
+pinMode(back_sen_two_echoPin, INPUT);
+
+
 Serial.begin(9600); // Starts the serial communication
 vehicalCount = 5;
 }
+
+
 void loop() {
 // Clears the trigPin
 
-d1 = senserOne();
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-// Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-// Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
-// Calculating the distance
-distance= duration*0.034/2;
-// Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
-Serial.println(distance);
+front_sen_one_distance = front_sen_one();
+front_sen_two_distance = front_sen_two();
+back_sen_one_distance  = back_sen_one();
+back_sen_two_distance = back_sen_two();
+
 }
 
-int senserOne(){
+int front_sen_one(){
+  int distance=0;
   // Clears the trigPin
-digitalWrite(trigPin, LOW);
+digitalWrite(front_sen_one_trigPin, LOW);
 delayMicroseconds(2);
 // Sets the trigPin on HIGH state for 10 micro seconds
-digitalWrite(trigPin, HIGH);
+digitalWrite(front_sen_one_trigPin, HIGH);
 delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
+digitalWrite(front_sen_one_trigPin, LOW);
 // Reads the echoPin, returns the sound wave travel time in microseconds
-duration = pulseIn(echoPin, HIGH);
+front_sen_one_duration = pulseIn(front_sen_one_echoPin, HIGH);
 // Calculating the distance
-distance= duration*0.034/2;
+distance= front_sen_one_duration*0.034/2;
 // Prints the distance on the Serial Monitor
-Serial.print("Distance: ");
+Serial.print("Front Sensor One Distance: ");
+Serial.println(distance);
+return distance;
+  }
+
+  int front_sen_two(){
+    int distance=0;
+  // Clears the trigPin
+digitalWrite(front_sen_one_trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(front_sen_one_trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(front_sen_one_trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+front_sen_two_duration = pulseIn(front_sen_two_echoPin, HIGH);
+// Calculating the distance
+distance= front_sen_two_duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Front Sensor Two Distance: ");
 Serial.println(distance);
 
-return distance
+return distance;
   }
+
+int back_sen_one(){
+  int distance=0;
+  // Clears the trigPin
+digitalWrite(front_sen_one_trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(front_sen_one_trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(front_sen_one_trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+back_sen_one_duration = pulseIn(back_sen_one_echoPin, HIGH);
+// Calculating the distance
+distance= back_sen_one_duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Back Sensor One Distance: ");
+Serial.println(distance);
+
+return distance;
+  }
+
+int back_sen_two(){
+    int distance=0;
+  // Clears the trigPin
+digitalWrite(front_sen_one_trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(front_sen_one_trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(front_sen_one_trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+back_sen_two_duration = pulseIn(back_sen_two_echoPin, HIGH);
+// Calculating the distance
+distance= back_sen_two_duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Back Sensor Two Distance: ");
+Serial.println(distance);
+
+return distance;
+  }
+
+  
 
 
 
