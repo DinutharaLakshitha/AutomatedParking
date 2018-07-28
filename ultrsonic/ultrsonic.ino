2379cca4c1ac9e3d3de1b2af56e92275e8c36709
DinutharaLakshitha <dinutharalakshitha@gmail.com>
2018-07-28 11:05:11 +0530

    Ultrasonic code changes

--- ultrsonic/ultrsonic.ino
+++ ultrsonic/ultrsonic.ino
@@ -11,13 +11,17 @@ const int echoPin = 10;
 // defines variables
 long duration;
 int distance;
+int vehicalCount;
 void setup() {
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 Serial.begin(9600); // Starts the serial communication
+vehicalCount = 5;
 }
 void loop() {
 // Clears the trigPin
+
+d1 = senserOne();
 digitalWrite(trigPin, LOW);
 delayMicroseconds(2);
 // Sets the trigPin on HIGH state for 10 micro seconds
@@ -32,3 +36,25 @@ distance= duration*0.034/2;
 Serial.print("Distance: ");
 Serial.println(distance);
 }
+
+int senserOne(){
+  // Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+
+return distance
+  }
+
+
+
