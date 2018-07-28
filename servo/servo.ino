919f1074ca9be87853390af127c3ee23329a7a65
Lakshitha Rajapakse <lakshitha.15@cse.mrt.ac.lk>
2018-07-28 11:05:28 +0530

    edited servo

--- servo/servo.ino
+++ servo/servo.ino
@@ -4,10 +4,12 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 void setup() {
  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+ Serial.begin(9600);
 }
 void loop() {
  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees
  {                                  // in steps of 1 degree
+    Serial.print(pos);
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
  }
