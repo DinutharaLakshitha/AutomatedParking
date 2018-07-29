0fe340e04b19dc70228d7a0b5a99df1aad2a50f8
DinutharaLakshitha <dinutharalakshitha@gmail.com>
2018-07-29 15:00:41 +0530

    bugs fixes

--- parking/parking.ino
+++ parking/parking.ino
@@ -33,7 +33,11 @@ int vehicalCount;
 int posGate1 = 0;
 int posGate2 = 0;
 
-char isUserValid;
+char _isUserValid;
+
+int carParkFull = 11;
+int AccessGranded = 12;
+int AccessDenide = 13;
 
 void setup() {
   pinMode(front_sen_one_trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -56,9 +60,11 @@ void setup() {
   Gate1servo.write(0);
   Gate2servo.write(0);
 
-  pinMode(12, OUTPUT);
+  pinMode(carParkFull, OUTPUT);
+  pinMode(AccessGranded,OUTPUT);
+  pinMode(AccessDenide,OUTPUT);
 
-  isUserValid = 0;
+  _isUserValid = "0";
 }
 
 
@@ -67,42 +73,53 @@ void loop() {
   int timer1;
   int timer2;
 
-//  front_sen_one_distance = front_sen_one();
-//  delay(500);
-//  front_sen_two_distance = front_sen_two();
-//  delay(500);
-//   back_sen_one_distance  = back_sen_one();
-//   delay(500);
-//    back_sen_two_distance = back_sen_two();
-//    delay(500);
+  //  front_sen_one_distance = front_sen_one();
+  //  delay(500);
+  //  front_sen_two_distance = front_sen_two();
+  //  delay(500);
+  //   back_sen_one_distance  = back_sen_one();
+  //   delay(500);
+  //    back_sen_two_distance = back_sen_two();
+  //    delay(500);
 
-  if (front_sen_one() <= 5 && front_sen_one() > 0) {
-    if (vehicalCount > 0 && (isUserValid()=="1") ) {
-      openGate1();
-      for ( timer1 = 0 ; timer1 < 10 ; timer1++) {
-        delay(2000);
-        if (front_sen_two() <= 5 && front_sen_two() > 0) {
+  if (front_sen_one() <= 5 && front_sen_one() > 1) {
+    if (vehicalCount > 0) {
+      while (isUserValid() == "0"){
+        Serial.println("Im in the while loop");
+        }
+      
+      if (_isUserValid == "1") {
+        openGate1();
+        for ( timer1 = 0 ; timer1 < 10 ; timer1++) {
+          delay(2000);
+          if (front_sen_two() <= 5 && front_sen_two() > 1) {
+            closeGate1();
+            vehicalCount--;
+            timer1 = 20;
+          }
+        }
+        if (timer1 != 21) {
           closeGate1();
-          vehicalCount--;
-          timer1 = 20;
         }
       }
-      if (timer1 != 21) {
-        closeGate1();
+      else if (_isUserValid == "2") {
+        digitalWrite(AccessDenide,HIGH);
+        delay(5000);
+        digitalWrite(AccessDenide,LOW);
       }
     }
     else {
-      digitalWrite(12 , HIGH);
+      digitalWrite(carParkFull , HIGH);
     }
   }
   else {
-    digitalWrite(12, LOW);
+    digitalWrite(carParkFull, LOW);
     delay(100);
-    if (back_sen_one() <= 5 && back_sen_one() > 0) {
+    if (back_sen_one() <= 5 && back_sen_one() > 1) {
       openGate2();
       for ( timer2 = 0 ; timer2 < 10 ; timer2++) {
         delay(2000);
-        if (back_sen_two() <= 5 && back_sen_two() > 0) {
+        if (back_sen_two() <= 5 && back_sen_two() > 1) {
           closeGate2();
           vehicalCount++;
           timer2 = 20;
@@ -198,7 +215,7 @@ int back_sen_two() {
 }
 
 void openGate1() {
-
+  digitalWrite(AccessGranded,HIGH);
   for (posGate1 = 0; posGate1 <= 60; posGate1 += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     Gate1servo.write(posGate1);              // tell servo to go to position in variable 'pos'
@@ -212,6 +229,7 @@ void closeGate1() {
     Gate1servo.write(posGate1);              // tell servo to go to position in variable 'pos'
     delay(30);                       // waits 15ms for the servo to reach the position
   }
+  digitalWrite(AccessGranded,LOW);
 }
 
 void openGate2() {
@@ -231,13 +249,13 @@ void closeGate2() {
   }
 }
 
-char isUserValid(){
-  if(Serial.available()){
-    isUserValid = Serial.read();
-    Serial.print(isUserValid);
-    return isUserValid:
-    }else{
-      return "not scanned"
-      }
-  
+char isUserValid() {
+  if (Serial.available()) {
+    _isUserValid = Serial.read();
+    Serial.print(_isUserValid);
+return _isUserValid;
+  } else {
+    return "0";
   }
+
+}
