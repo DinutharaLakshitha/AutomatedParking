a205d9e7cf69b899acb64f06a17a30dbde6f2616
DinutharaLakshitha <dinutharalakshitha@gmail.com>
2018-07-30 14:25:51 +0530

    final code

--- parking/parking.ino
+++ parking/parking.ino
@@ -1,3 +1,4 @@
+
 #include <Servo.h>
 
 Servo Gate1servo;
@@ -6,16 +7,9 @@ Servo Gate2servo;
 // defines pins numbers
 const int front_sen_one_trigPin = 2;
 const int front_sen_one_echoPin = 3;
-
-//const int front_sen_two_trigPin = 4;
 const int front_sen_two_echoPin = 4;
-
-//const int back_sen_one_trigPin = 7;
 const int back_sen_one_echoPin = 5;
-
-//const int back_sen_two_trigPin = 8;
 const int back_sen_two_echoPin = 6;
-
 
 // defines variables
 long front_sen_one_duration;
@@ -36,7 +30,7 @@ int posGate2 = 0;
 char _isUserValid;
 
 int carParkFull = 11;
-int AccessGranded = 12;
+int scanQR = 12;
 int AccessDenide = 13;
 
 void setup() {
@@ -55,40 +49,31 @@ void setup() {
 
 
   Serial.begin(9600); // Starts the serial communication
-  vehicalCount = 3;
+  vehicalCount = 3; // maximum vehicle capacity
 
   Gate1servo.write(0);
   Gate2servo.write(0);
 
   pinMode(carParkFull, OUTPUT);
-  pinMode(AccessGranded,OUTPUT);
-  pinMode(AccessDenide,OUTPUT);
+  pinMode(scanQR, OUTPUT);
+  pinMode(AccessDenide, OUTPUT);
 
-  _isUserValid = "0";
+  _isUserValid = '0';
 }
 
 
 void loop() {
-  // Clears the trigPin
+  //init the timers
   int timer1;
   int timer2;
 
-  //  front_sen_one_distance = front_sen_one();
-  //  delay(500);
-  //  front_sen_two_distance = front_sen_two();
-  //  delay(500);
-  //   back_sen_one_distance  = back_sen_one();
-  //   delay(500);
-  //    back_sen_two_distance = back_sen_two();
-  //    delay(500);
-
-  if (front_sen_one() <= 5 && front_sen_one() > 1) {
+  if (front_sen_one() <= 5 && front_sen_one() > 1) { // check there is a vehicle in front gate
     if (vehicalCount > 0) {
-      while (isUserValid() == "0"){
-        Serial.println("Im in the while loop");
-        }
-      
-      if (_isUserValid == "1") {
+      while (isUserValid() == '0') { //wait till user scan the QR code
+        digitalWrite(scanQR, HIGH);
+      }
+      digitalWrite(scanQR, LOW);
+      if (_isUserValid == '1') { // If user is valod then gate will open
         openGate1();
         for ( timer1 = 0 ; timer1 < 10 ; timer1++) {
           delay(2000);
@@ -98,14 +83,14 @@ void loop() {
             timer1 = 20;
           }
         }
-        if (timer1 != 21) {
+        if (timer1 != 21) {//if the vehicle do not get in to the car park then the gate will automatically close after 20sec wait
           closeGate1();
         }
       }
-      else if (_isUserValid == "2") {
-        digitalWrite(AccessDenide,HIGH);
+      else if (_isUserValid == '2') {//if user not valid gate will not open
+        digitalWrite(AccessDenide, HIGH);
         delay(5000);
-        digitalWrite(AccessDenide,LOW);
+        digitalWrite(AccessDenide, LOW);
       }
     }
     else {
@@ -115,7 +100,7 @@ void loop() {
   else {
     digitalWrite(carParkFull, LOW);
     delay(100);
-    if (back_sen_one() <= 5 && back_sen_one() > 1) {
+    if (back_sen_one() <= 5 && back_sen_one() > 1) {//back gate check
       openGate2();
       for ( timer2 = 0 ; timer2 < 10 ; timer2++) {
         delay(2000);
@@ -148,9 +133,7 @@ int front_sen_one() {
   front_sen_one_duration = pulseIn(front_sen_one_echoPin, HIGH);
   // Calculating the distance
   distance = front_sen_one_duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Front Sensor One Distance: ");
-  Serial.println(distance);
+
   return distance;
 }
 
@@ -167,9 +150,6 @@ int front_sen_two() {
   front_sen_two_duration = pulseIn(front_sen_two_echoPin, HIGH);
   // Calculating the distance
   distance = front_sen_two_duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Front Sensor Two Distance: ");
-  Serial.println(distance);
 
   return distance;
 }
@@ -187,9 +167,6 @@ int back_sen_one() {
   back_sen_one_duration = pulseIn(back_sen_one_echoPin, HIGH);
   // Calculating the distance
   distance = back_sen_one_duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Back Sensor One Distance: ");
-  Serial.println(distance);
 
   return distance;
 }
@@ -207,45 +184,39 @@ int back_sen_two() {
   back_sen_two_duration = pulseIn(back_sen_two_echoPin, HIGH);
   // Calculating the distance
   distance = back_sen_two_duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Back Sensor Two Distance: ");
-  Serial.println(distance);
 
   return distance;
 }
 
 void openGate1() {
-  digitalWrite(AccessGranded,HIGH);
-  for (posGate1 = 0; posGate1 <= 60; posGate1 += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    Gate1servo.write(posGate1);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+  for (posGate1 = 0; posGate1 <= 60; posGate1 += 1) { // goes from 0 degrees to 90 degrees
+    Gate1servo.write(posGate1);              // tell servo to go to position in variable 'posGate1'
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 
 void closeGate1() {
 
-  for (posGate1 = 60; posGate1 >= 1; posGate1 -= 1) { // goes from 180 degrees to 0 degrees
-    Gate1servo.write(posGate1);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+  for (posGate1 = 60; posGate1 >= 1; posGate1 -= 1) { // goes from 90 degrees to 0 degrees
+    Gate1servo.write(posGate1);              // tell servo to go to position in variable 'posGate1'
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
-  digitalWrite(AccessGranded,LOW);
 }
 
 void openGate2() {
 
-  for (posGate2 = 0; posGate2 <= 60; posGate2 += 1) { // goes from 0 degrees to 180 degrees
+  for (posGate2 = 0; posGate2 <= 60; posGate2 += 1) { // goes from 0 degrees to 90 degrees
     // in steps of 1 degree
-    Gate2servo.write(posGate2);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+    Gate2servo.write(posGate2);              // tell servo to go to position in variable 'posGate2'
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 
 void closeGate2() {
 
-  for (posGate2 = 60; posGate2 >= 1; posGate2 -= 1) { // goes from 180 degrees to 0 degrees
-    Gate2servo.write(posGate2);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+  for (posGate2 = 60; posGate2 >= 1; posGate2 -= 1) { // goes from 90 degrees to 0 degrees
+    Gate2servo.write(posGate2);              // tell servo to go to position in variable 'posGate2'
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 
@@ -253,9 +224,9 @@ char isUserValid() {
   if (Serial.available()) {
     _isUserValid = Serial.read();
     Serial.print(_isUserValid);
-return _isUserValid;
+    return _isUserValid;
   } else {
-    return "0";
+    return _isUserValid;
   }
 
 }
